c7a3c9bd62fd92c966bcfc26b23bacba3fb49fa3
Andrii Terytsia <a-aterytsia@microsoft.com>
2022-11-30 13:28:43 +0000

    create form

--- src/pages/create/Create.js
+++ src/pages/create/Create.js
@@ -1,10 +1,53 @@
 import './Create.css'
 
-import React from 'react'
+import React, {useState} from 'react'
 
 const Create = () => {
+    const [title, setTitle] = useState('')
+    const [method, setMethod] = useState('')
+    const [cookingTime, setCookingTime] = useState('')
+
+    const handleSubmit = (e) => {
+        e.preventDefault()
+        console.log(title, method, cookingTime)
+      }
   return (
-    <div>create</div>
+    <div className='create'>
+        <h2 className='page-title'>Add a NewRecipe</h2>
+
+        <form onSubmit={handleSubmit}>
+            <label>
+                <span>Recipe title:</span>
+                <input
+                    type="text"
+                    onChange={(e) => setTitle(e.target.value)}
+                    value={title}
+                    required
+                />
+            </label>
+            <label>
+                <span>Recipe method:</span>
+                <input
+                    type="text"
+                    onChange={(e) => setMethod(e.target.value)}
+                    value={method}
+                    required
+                />
+            </label>
+
+            <label>
+                <span>Cooking time (minutes):</span>
+                <input
+                    type="number"
+                    onChange={(e) => setCookingTime(e.target.value)}
+                    value={cookingTime}
+                    required
+                />
+            </label>
+            <button className="btn">submit</button>
+        </form>
+
+    </div>
   )
 }
 export default Create
