98a9e5287839dff9dce477d5ebbdf35835762072
Andrii Terytsia <a-aterytsia@microsoft.com>
2022-11-30 11:12:13 +0000

    styles to navbar

--- src/components/navbar/Navbar.js
+++ src/components/navbar/Navbar.js
@@ -7,7 +7,7 @@ export const Navbar = () => {
   return (
     <div className="navbar">
         <nav>
-            <NavLink  to="/"> <h1>My Cooking</h1> </NavLink>
+            <NavLink  to="/" className="brand"> <h1 >My Cooking</h1> </NavLink>
             <NavLink  to="/create"> Create Recipe </NavLink>
         </nav>
     </div>
